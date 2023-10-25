7606f3f2d0f2ebbb7b2ec86d19933976d95d1f99
unknown <piuspio12@gmail.com>
2023-10-25 03:49:05 +0100

    added id in notification model

--- notification/models.py
+++ notification/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from authentication.models import User
+import uuid
 
 class Notifications(models.Model):
+    id = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4)
     title = models.CharField(max_length=200)
     user = models.ForeignKey(User, on_delete=models.CASCADE,default='')
     message = models.CharField(max_length=500)
