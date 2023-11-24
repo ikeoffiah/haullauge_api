c2e8bab944610f3f2e54b460c9462dd41f7cc213
unknown <piuspio12@gmail.com>
2023-11-24 16:04:48 +0100

    modified pricing

--- bookings/utils.py
+++ bookings/utils.py
@@ -92,22 +92,22 @@ def lessthan10(distance, load, fuel_price):
 
 def others(distance, load, fuel_price):
     if distance < 50:
-        price = (load * distance * fuel_price)
+        price = (load * distance * fuel_price)/1.5
         return price
     if distance >49 and distance < 100:
-        price = (load * distance * fuel_price)/6.5
+        price = (load * distance * fuel_price)/4.5
         return price
     if distance >99 and distance < 200:
-        price = (load * distance * fuel_price)/8.5
+        price = (load * distance * fuel_price)/6.5
         return price
     if distance > 199 and distance < 400:
-        price = (load * distance * fuel_price)/10.5
+        price = (load * distance * fuel_price)/8.5
         return price
     if distance > 399 and distance < 800:
-        price = (load * distance * fuel_price) / 12.5
+        price = (load * distance * fuel_price) / 10.5
         return price
     if distance > 799:
-        price = (load * distance * fuel_price) / 15.5
+        price = (load * distance * fuel_price) / 12.5
         return price
 
 
